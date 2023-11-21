17d7fd24e913b34cfb7f5d5f55944e009da697c6
BlackCCCat <360252749zzx@gmail.com>
2023-11-21 14:46:03 +0800

    add guess-number game code

--- guess-number/main.py
+++ guess-number/main.py
@@ -1,3 +1,27 @@
+import random
+
+def main():
+    scale = input('Choose a number between 1 and ? :')
+    number = random.randint(1, int(scale))
+    game = True
+    while game:
+        guess = input('Enter your guess: ')
+        guess = int(guess)
+
+        if guess > number:
+            print('Too high')
+        elif guess < number:
+            print('Too low')
+        else:
+            print('Correct')
+            game = False
+    
+    restart = input('Would you like to play again? (y/n)')
+    if restart == 'y':
+        main()
+    else:
+        print('Goodbye!')
+
 
 if __name__ == '__main__':
-    pass
+    main()
